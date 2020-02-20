f364760b037b6680a5ffe17acca61e359906d144
Nazar Z <nazarzastavnyi@gmail.com>
2020-02-20 15:23:05 +0200

    DEV: change admin model

--- app/models/admin.js
+++ app/models/admin.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const session_constants = require('../constants/session');
 
 module.exports = {
     model_name: 'Admin',
@@ -11,6 +12,10 @@ module.exports = {
         reset_password_token:       { type: Sequelize.STRING, required: false },
         reset_password_expires_at:  { type: Sequelize.BIGINT, required: false },
         role:                       { type: Sequelize.STRING, required: false },
+        session_token:          { type: Sequelize.STRING, allowNull: false },
+        refresh_token:          { type: Sequelize.STRING, allowNull: false },
+        status:                 { type: Sequelize.SMALLINT, allowNull: false, defaultValue: session_constants.status.ACTIVE },
+        expires_at:             { type: Sequelize.BIGINT },
 
     },
 };
