73168d44dabc2d8f272d8f068f13d8bf01ad9aed
Nazar Z <nazarzastavnyi@gmail.com>
2020-02-21 11:48:47 +0200

    DEV: fix auto replace

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [
         {
             name: 'predictus_server',
-            script: 'auth.js.js',
+            script: 'index.js',
             instances: 1,
             autorestart: true,
             watch: false,
